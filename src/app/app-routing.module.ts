d658d9b63f39ac94babd8879a5f3206c7283d002
leo-demetrio <leocdemetrio@gmail.com>
2021-10-27 10:39:51 -0300

    feat-3: login integred auth.service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,11 @@ import { HomeComponent } from './views/home/home.component';
 import { ProductComponent } from './views/product/product.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
-// import { LoginComponent } from './core/components/login/login.component';
 
 const routes: Routes = [
-  // {
-  //   path: "login",
-  //   component: LoginComponent
-  // },
   {
     path: "",
     component: HomeComponent
-    // component: LoginComponent
   },
   {
     path: "products",
